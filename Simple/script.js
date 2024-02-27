825ac3a099335aa4f02e0b3305fe089796b85c76
GuilhermeMosson <guimosson@gmail.com>
2024-02-27 10:57:11 -0300

    Aulão 27/02

---
+++ Simple/script.js
@@ -0,0 +1,7 @@
+function calcular(){
+    var n1 = document.getElementById("num1").value;
+    var n2 = document.getElementById("num2").value;
+    var resul = document.getElementById("res");
+
+    console.log(n1, n2, resul)
+}
